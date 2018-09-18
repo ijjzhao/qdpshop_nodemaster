879d1f57ef9054bed0dec96833c4ce72cc68dfd5
Qiu Hong <qiuh1016@126.com>
2018-09-18 20:35:10 +0800

    更新微信id

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -3,8 +3,8 @@ module.exports = {
   default_module: 'api',
   workers: 1,
   weixin: {
-    appid: 'wxb2c22856549f3ab2', // 小程序 appid
-    secret: '34268d31ff74199a69a7b5d017d8b450', // 小程序密钥
+    appid: 'wx703fd25a2cfce741', // 小程序 appid
+    secret: 'df6f71acbff56fddf7ad1c027786c6d1', // 小程序密钥
     mch_id: '', // 商户帐号ID
     partner_key: '', // 微信支付密钥
     notify_url: '' // 微信异步通知，
