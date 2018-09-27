631785a5d3017689d33850630b990d7e87ebeea0
Qiu Hong <qiuh1016@126.com>
2018-09-27 19:35:52 +1000

    微信卡券核销

--- src/api/controller/order.js
+++ src/api/controller/order.js
@@ -10,18 +10,18 @@ module.exports = class extends Base {
     const index = this.post('index')
     let state = ''
     // let orderListno = []
-    if(parseInt(index) == 0){state = 0} // 如果订单没有被取消，且没有支付，则可支付，可取消
-    if(parseInt(index) == 1){state = 201} //如果订单已付款，没有发货，则可退款操作
-    if(parseInt(index) == 2){state = 300}  // 如果订单已经发货，没有收货，则可收货操作和退款、退货操作
-    if(parseInt(index) == 3){state = 301}  // 如果订单已经支付，且已经收货，则可完成交易、评论和再次购买  //已完成
-    if(parseInt(index) == 4){
-      const refundList = await this.model('order_refund').where({ user_id: think.userId,abled:0,isRefund:0,state:1001}).select();
+    if (parseInt(index) == 0) { state = 0 } // 如果订单没有被取消，且没有支付，则可支付，可取消
+    if (parseInt(index) == 1) { state = 201 } //如果订单已付款，没有发货，则可退款操作
+    if (parseInt(index) == 2) { state = 300 }  // 如果订单已经发货，没有收货，则可收货操作和退款、退货操作
+    if (parseInt(index) == 3) { state = 301 }  // 如果订单已经支付，且已经收货，则可完成交易、评论和再次购买  //已完成
+    if (parseInt(index) == 4) {
+      const refundList = await this.model('order_refund').where({ user_id: think.userId, abled: 0, isRefund: 0, state: 1001 }).select();
       console.log(refundList);
       let list = []
       for (var i = 0; i < refundList.length; i++) {
         // array[i]
         let obj = {}
-        obj = await this.model('order').where({ order_sn: refundList[i].order_sn,is_del:0}).find();
+        obj = await this.model('order').where({ order_sn: refundList[i].order_sn, is_del: 0 }).find();
         obj.refund_sn = refundList[i].refund_sn
         obj.state = refundList[i].state
         obj.state_text = refundList[i].state_text
@@ -49,10 +49,10 @@ module.exports = class extends Base {
       }
       orderList.data = newOrderList;
       return this.success(orderList);
-       // 退款中 售后中
-    }else {
+      // 退款中 售后中
+    } else {
 
-      const orderList = await this.model('order').where({ user_id: think.userId,order_status:state,is_del:0}).select();
+      const orderList = await this.model('order').where({ user_id: think.userId, order_status: state, is_del: 0 }).select();
       // console.log(orderList);
       const newOrderList = [];
       for (const item of orderList) {
@@ -95,9 +95,9 @@ module.exports = class extends Base {
     console.log(sn);
     const orderInfo = await this.model('order').where({ order_sn: sn }).find();
     if (orderInfo.order_status == 0) {
-      return this.fail(17,'订单未支付')
-    }else {
-      return this.fail(506,'订单已支付')
+      return this.fail(17, '订单未支付')
+    } else {
+      return this.fail(506, '订单已支付')
 
     }
 
@@ -105,7 +105,7 @@ module.exports = class extends Base {
   async detailAction() {
     const orderId = this.get('orderId');
     const orderInfo = await this.model('order').where({ id: orderId }).find();
-    const refundInfo = await this.model('order_refund').where({order_sn:orderInfo.order_sn}).select()
+    const refundInfo = await this.model('order_refund').where({ order_sn: orderInfo.order_sn }).select()
     if (think.isEmpty(orderInfo)) {
       return this.fail('订单不存在');
     }
@@ -147,25 +147,25 @@ module.exports = class extends Base {
    * 取消订单
    * @returns {Promise.<void>}
    */
-   async cancelAction() {
-     const orderId = this.get("orderId")
-     // DELETE FROM 表名称 WHERE 列名称 = 值
-     // const cancelorderList = await this.model('order').where({ user_id: think.userId, id: orderId }).update({ order_status: "1"});
-     const cancelorderList = await this.model('order').where({ user_id: think.userId, id: orderId }).update({is_del:1});
-     let pruids = await this.model('order_goods').where({order_id:orderId}).select()
-     for (var i = 0; i < pruids.length; i++) {
-       // array[i]
-       let productlis = await this.model('product').where({id:pruids[i].product_id}).find()
-       await this.model('product').where({id:pruids[i].product_id}).update({
-         goods_number:parseInt(productlis.goods_number)+1
-       })
+  async cancelAction() {
+    const orderId = this.get("orderId")
+    // DELETE FROM 表名称 WHERE 列名称 = 值
+    // const cancelorderList = await this.model('order').where({ user_id: think.userId, id: orderId }).update({ order_status: "1"});
+    const cancelorderList = await this.model('order').where({ user_id: think.userId, id: orderId }).update({ is_del: 1 });
+    let pruids = await this.model('order_goods').where({ order_id: orderId }).select()
+    for (var i = 0; i < pruids.length; i++) {
+      // array[i]
+      let productlis = await this.model('product').where({ id: pruids[i].product_id }).find()
+      await this.model('product').where({ id: pruids[i].product_id }).update({
+        goods_number: parseInt(productlis.goods_number) + 1
+      })
 
-     }
-     return this.success({
-       orderId,cancelorderList
+    }
+    return this.success({
+      orderId, cancelorderList
 
-     })
-   }
+    })
+  }
   /**
    * 提交订单
    * @returns {Promise.<void>}
@@ -189,8 +189,8 @@ module.exports = class extends Base {
     if (think.isEmpty(checkedAddress)) {
       return this.fail('请选择收货地址');
     }
-    console.log("*********************************************************");
-    console.log(IdentityInput);
+    // console.log("*********************************************************");
+    // console.log(IdentityInput);
 
     // 获取要购买的商品
     const checkedGoodsList = await this.model('cart').where({ user_id: userId, session_id: 1, checked: 1 }).select();
@@ -205,24 +205,41 @@ module.exports = class extends Base {
     if (couponId == 0) {
       // let checkcup = []
       console.log('没有选择优惠券');
-    }else {
+    } else {
       console.log('选择了优惠券');
       // let checkcup = await this.model('coupon_user').where({user_id: userId,coupon_id:couponId}).find()
     }
-    const checkedcouponlist = couponId == 0 ? [] : await this.model('coupon_user').where({coupon_id:couponId}).find()
-    console.log(checkedcouponlist);
+    const checkedcouponlist = couponId == 0 ? [] : await this.model('coupon_user').where({ coupon_id: couponId }).find()
+    // console.log(checkedcouponlist);
     const currentTime = new Date().getTime()
-    for (var p = 0;p < GoodsList.length; p++){
-      const product = await this.model('product').where({id:GoodsList[p].product_id}).find()
-      await this.model('product').where({id:GoodsList[p].product_id}).update({
-        goods_number:parseInt(product.goods_number) - 1
+
+    // 减少商品库存
+    for (var p = 0; p < GoodsList.length; p++) {
+      const product = await this.model('product').where({ id: GoodsList[p].product_id }).find()
+      await this.model('product').where({ id: GoodsList[p].product_id }).update({
+        goods_number: parseInt(product.goods_number) - 1
       })
     }
-    let couponUpdate = couponId == 0 ? '' : await this.model('coupon_user').where({coupon_id:couponId}).update({
+
+    // 使用优惠券
+    let couponUpdate = couponId == 0 ? '' : await this.model('coupon_user').where({ coupon_id: couponId }).update({
       used_type: 1,
       used_time: new Date().getTime()
     })
-    console.log(couponUpdate);
+
+    // TODO：如果是微信的卡券 还要核销 ? 下单了没有付钱 卡券怎么办
+    let card = await this.model('coupon_user').where({ coupon_id: couponId }).find();
+    if (card.code) {
+      let service = this.service('wxcard', 'api');
+      if (await service.checkCode(card.code) && await service.consumeCode(card.code)) {
+        console.log('微信卡券正常核销')
+      } else {
+        return this.fail('卡券状态异常');
+      }
+    }
+
+    // console.log(couponUpdate);
+
     const orderInfo = {
       order_sn: this.model('order').generateOrderNumber(),
       user_id: think.userId,
@@ -241,7 +258,7 @@ module.exports = class extends Base {
       postscript: postscript,
       // 使用的优惠券
       coupon_id: couponId,
-      checkedcoupon_list:checkedcouponlist,
+      checkedcoupon_list: checkedcouponlist,
       coupon_price: couponPrice,
       user_level_discount: userDiscount_Price,
       add_time: currentTime,
@@ -249,7 +266,7 @@ module.exports = class extends Base {
       order_price: orderTotalPrice,
       actual_price: actualPrice
     };
-    console.log(orderInfo);
+    // console.log(orderInfo);
     //开启事务，插入订单信息和订单商品
     const orderId = await this.model('order').add(orderInfo);
     orderInfo.id = orderId;
@@ -285,17 +302,16 @@ module.exports = class extends Base {
     return this.success({ orderInfo: orderInfo });
   }
 
-
-/**
- * 查询
- * @returns {Promise.<void>}
- */
-async expressAction() {
-  const orderId = this.get('orderId');
-  if (think.isEmpty(orderId)) {
-    return this.fail('订单不存在');
+  /**
+   * 查询
+   * @returns {Promise.<void>}
+   */
+  async expressAction() {
+    const orderId = this.get('orderId');
+    if (think.isEmpty(orderId)) {
+      return this.fail('订单不存在');
+    }
+    const latestExpressInfo = await this.model('order_express').getLatestOrderExpress(orderId);
+    return this.success(latestExpressInfo);
   }
-  const latestExpressInfo = await this.model('order_express').getLatestOrderExpress(orderId);
-  return this.success(latestExpressInfo);
-}
 };
